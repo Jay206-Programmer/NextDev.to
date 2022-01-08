06ff502db3efc679711e0f932585cf691dc9fdbf
Jay Shukla <jayshukla0034@gmail.com>
2022-01-08 08:42:12 -0800

    firebase-auth setup midway

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,43 @@
-export default function EnterPage() {
+import { auth, signIn, googleAuthProvider } from '../lib/firebase';
+import Image from 'next/image';
+import { FcGoogle } from "react-icons/fc"
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-      <h1>Sign In</h1>
+      {user ? 
+        !username ? <UsernameForm /> : <SignOutButton /> 
+        : 
+        <SignInButton />
+      }
     </main>
-  )
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signIn(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <div className="icon"><FcGoogle /></div> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@
 // import Image from "next/image";
 import Link from "next/link";
 
+//* Third-Party Libraries
+import toast from "react-hot-toast";
+
 //* Importing Components
-import Loader from "../components/Loader"; 
+import Loader from "../components/Loader";
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      {/* <Loader show /> */}
+      <button onClick={() => toast.success("Success!")}>click me</button>
     </div>
   );
 }
