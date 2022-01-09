5853198b8eefdffafa2beb02d855e6eed8aa5980
Jay Shukla <jayshukla0034@gmail.com>
2022-01-09 07:23:15 -0800

    Sunday Push: Authentication Implemented

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from "next/link";
 
-// Top navbar
+//* Library Imports
+import Link from "next/link";
+import { useContext } from "react";
+
+//* Internal Imports
+import { UserContext } from "../lib/context";
+
 export default function Navbar() {
-  const user = null;
-  const username = null;
+  const { user, username } = useContext(UserContext);
+
+  const signOut = () => {
+    auth.signOut();
+    router.reload();
+  };
 
   return (
     <nav className="navbar">
       <ul>
         <li>
           <Link href="/" passHref>
-            <button className="btn-logo">FEED</button>
+            <button className="btn-logo">NXT</button>
           </Link>
         </li>
 
@@ -19,13 +28,17 @@ export default function Navbar() {
         {username && (
           <>
             <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+            <li>
               <Link href="/admin" passHref>
                 <button className="btn-blue">Write Posts</button>
               </Link>
             </li>
             <li>
               <Link href={`/${username}`} passHref>
-                <img src={user?.photoURL} alt="Image" />
+                {/* {console.log(user?user.photoURL:null)} */}
+                <img src={user?.photoURL || "/hacker.png"} alt="image" />
               </Link>
             </li>
           </>
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
+//* Importing CSS
 import "../styles/globals.css";
+
+//* Global Components
 import Navbar from "../components/Navbar";
+import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
+
+//* Third-party Libraries
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
     <>
-      <Toaster/>
-      <Navbar />
-      <Component {...pageProps} />
+      <UserContext.Provider value={userData}>
+        <Toaster />
+        <Navbar />
+        <Component {...pageProps} />
+      </UserContext.Provider>
     </>
   );
 }
